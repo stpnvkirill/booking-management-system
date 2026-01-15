13f0d64eb0dcb63ddcbdf312deab836c7b80ef51
marrrikk <jobmarru@gmail.com>
2026-01-15 19:25:04 +0700

    Database

--- app/infrastructure/database/models/users.py
+++ app/infrastructure/database/models/users.py
@@ -30,6 +30,11 @@ class User(BaseWithDt):
         UUID,
         server_default=sa.func.uuidv7(),
     )
+    customer_id: so.Mapped[uuid_lib.UUID] = so.mapped_column(
+        UUID,
+        sa.ForeignKey("customers.id", ondelete="CASCADE"),
+        nullable=False,
+    )
 
     def __repr__(self):
         return f"User #{self.id}"
@@ -95,5 +100,100 @@ class BotConfig(BaseWithDt):
         sa.ForeignKey(Customer.id),
         unique=True,
     )
-
+    customer_id: so.Mapped[uuid_lib.UUID] = so.mapped_column(
+        UUID,
+        sa.ForeignKey("customers.id", ondelete="CASCADE"),
+        nullable=False,
+    )
     settings: so.Mapped[dict] = so.mapped_column(JSONB, server_default="{}")
+
+
+class Resource(Base, CreatedMixin):
+    __tablename__ = "resources"
+
+    id: so.Mapped[int] = so.mapped_column(
+        sa.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+    name: so.Mapped[str] = so.mapped_column(sa.VARCHAR(255), nullable=False)
+    customer_id: so.Mapped[uuid_lib.UUID] = so.mapped_column(
+        UUID,
+        sa.ForeignKey("customers.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+
+
+class Booking(BaseWithDt):
+    __tablename__ = "bookings"
+
+    id: so.Mapped[int] = so.mapped_column(
+        sa.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+    resource_id: so.Mapped[int] = so.mapped_column(
+        sa.Integer,
+        sa.ForeignKey("resources.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    user_id: so.Mapped[uuid_lib.UUID] = so.mapped_column(
+        UUID,
+        sa.ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    start_time: so.Mapped[sa.DateTime] = so.mapped_column(
+        sa.DateTime(timezone=True),
+        nullable=False,
+    )
+    end_time: so.Mapped[sa.DateTime] = so.mapped_column(
+        sa.DateTime(timezone=True),
+        nullable=False,
+    )
+    customer_id: so.Mapped[uuid_lib.UUID] = so.mapped_column(
+        UUID,
+        sa.ForeignKey("customers.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+
+    __table_args__ = (
+        sa.UniqueConstraint(
+            "resource_id",
+            "start_time",
+            "end_time",
+            name="unique_booking",
+        ),
+    )
+
+
+class Feedback(BaseWithDt):
+    __tablename__ = "feedbacks"
+
+    id: so.Mapped[int] = so.mapped_column(
+        sa.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+    booking_id: so.Mapped[int] = so.mapped_column(
+        sa.Integer,
+        sa.ForeignKey("bookings.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    user_id: so.Mapped[uuid_lib.UUID] = so.mapped_column(
+        UUID,
+        sa.ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    comment: so.Mapped[str | None] = so.mapped_column(sa.Text)
+    rating: so.Mapped[int] = so.mapped_column(
+        sa.SmallInteger,
+        nullable=False,
+    )
+    customer_id: so.Mapped[uuid_lib.UUID] = so.mapped_column(
+        UUID,
+        sa.ForeignKey("customers.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+
+
+__table_args__ = (sa.CheckConstraint("rating BETWEEN 1 AND 5", name="rating_check"),)
