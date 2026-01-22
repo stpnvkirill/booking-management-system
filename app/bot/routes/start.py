0e222224746b70dd8ce887e33c75e4f07149aa00
Pavel <avelnet@outlook.com>
2026-01-22 18:21:28 +0700

    Update start.py
    update def get_start_router()

--- app/bot/routes/start.py
+++ app/bot/routes/start.py
@@ -8,11 +8,10 @@ from app.bot.keyboards.main_menu import get_main_menu
 router = Router()
 
 
-def get_start_router():
-    return router
+def get_start_router() -> Router:
+    router: Router = Router()
 
-
-@router.message()
+    @router.message(OnlyPrivateChatFilter())
 async def start_handler(message: Message):
     if message.text == "/start":
         await message.answer(
