177db6e86e66805966bbb06c2fc61ce81a4747c1
Pavel <avelnet@outlook.com>
2026-01-26 20:39:00 +0700

    fix and update app/bot

--- app/bot/routes/__init__.py
+++ app/bot/routes/__init__.py
@@ -1,5 +1,10 @@
 from aiogram import Router
 
+from app.bot.routes.button_handler import get_button_handler_router
+from app.bot.routes.bookings import get_bookings_router
+from app.bot.routes.reviews import get_reviews_router
+from app.bot.routes.settings import get_settings_router
+
 from .admin import create_admin_router
 from .echo import get_echo_router
 from .ping import get_ping_router
@@ -8,7 +13,10 @@ from .start import get_start_router
 
 def create_router() -> Router:
     router: Router = Router()
-
+    router.include_router(get_button_handler_router())
+    router.include_router(get_bookings_router())
+    router.include_router(get_settings_router())
+    router.include_router(get_reviews_router())
     router.include_router(get_start_router())
     router.include_router(get_ping_router())
     router.include_router(get_echo_router())
--- app/bot/routes/start.py
+++ app/bot/routes/start.py
@@ -1,9 +1,9 @@
 from aiogram import Router
-from aiogram.filters import command
 from aiogram.types import Message
+from aiogram.filters import command
+from app.bot.handler import handler
 
 from app.bot.filters.chat_type import OnlyPrivateChatFilter
-from app.bot.handler import handler
 from app.bot.keyboards.main_menu import get_main_menu
 
 
@@ -14,9 +14,10 @@ def get_start_router() -> Router:
     @handler
     async def start_handler(message: Message):
         await message.answer(
-            "👋 Привет!\n"
-            "Я помогу тебе забронировать ресурс, посмотреть твои бронирования и управлять настройками.",  # noqa: E501, RUF001
-            reply_markup=get_main_menu(),
-        )
-
+                "👋 Привет!\n"
+                "Я помогу тебе забронировать ресурс, " \
+                "посмотреть твои бронирования и управлять настройками.",
+                reply_markup=get_main_menu(),
+            )
+    
     return router
