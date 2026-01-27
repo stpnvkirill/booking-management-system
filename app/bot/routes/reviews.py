38b7f6360e38119115456191fef62baf79921427
Pavel <avelnet@outlook.com>
2026-01-27 16:33:14 +0700

    add fsm keyboard and connect db

---
+++ app/bot/routes/reviews.py
@@ -0,0 +1,20 @@
+from aiogram import Router, types
+from aiogram.fsm.context import FSMContext
+
+from app.bot.handler import handler
+from app.bot.keyboards.main_menu import get_main_menu
+
+
+def get_reviews_router() -> Router:
+    router: Router = Router()
+
+    @router.message(lambda m: m.text == "⭐️ Оставить отзыв")
+    @handler
+    async def start_reviews(message: types.Message, state: FSMContext):
+        await state.clear()
+        await message.answer(
+            "Функция отзывов временно недоступна. ",
+            reply_markup=get_main_menu(),
+        )
+
+    return router
