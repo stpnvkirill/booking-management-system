ef1aca2a831fd4810a8c2102e39ef953afab72f7
1mposs1blyt <alexandr.bryaginya@gmail.com>
2026-01-27 20:00:34 +0700

    фикс еще одной ошибки

--- app/infrastructure/database/models/__init__.py
+++ app/infrastructure/database/models/__init__.py
@@ -1,4 +1,4 @@
-from .booking import Booking, BookingType, Resource
+from .booking import Booking, Resource
 from .feedback import Feedback
 from .shared import Base
 from .users import BotConfig, Customer, CustomerAdmin, CustomerMember, User, UserBot
