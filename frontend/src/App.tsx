a5811243d2d190ceeaf43a2ed0ef964a5159ef34
1mposs1blyt <alexandr.bryaginya@gmail.com>
2026-01-29 14:53:36 +0700

    не готовая авторизация

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,40 @@ import { AuthContainer } from '@/features/auth/auth.tsx';
 import Resources from './pages/resources-page/resources-page';
 
 import Navbar from '@/features/navbar/navbar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Calendar from './pages/calendar-page/calendar-page';
 import Profile from './pages/profile-page/profile-page';
-// import type { BookingItem } from './shared/types/types';
+import axios from 'axios';
+import { Spinner } from './shared/components/spinner/spinner';
+
+const useAuth = () => {
+  // Состояние, указывающее, авторизован ли пользователь
+  const [isAuth, setIsAuth] = useState(false);
+
+  // Функция для отправки данных на сервер и получения статуса аутентификации
+  const signIn = async (initData?: string | object) => {
+    const { data } = await axios.post<boolean>(
+      'https://example.com/auth/signin', // URL эндпоинта аутентификации
+      { initData }, // Передаем данные для входа
+    );
+    setIsAuth(data); // Устанавливаем статус аутентификации
+  };
+
+  return { isAuth, signIn };
+};
+
 export default function App() {
   window.Telegram?.WebApp?.ready();
   type Tabs = 'resources' | 'calendar' | 'profile';
   const user: boolean = true;
-  const isAuthenticated: boolean = true;
+  const isAuthenticated: boolean = true;    
   const [activeTab, setActiveTab] = useState<Tabs>('resources');
-
+  const { isAuth, signIn } = useAuth();
+  useEffect(() => {
+    // Вызываем signIn при монтировании компонента,
+    // передавая initData из Telegram WebApp API
+    signIn(window?.Telegram?.WebApp?.initDataUnsafe?.user);
+  }, []);
   // const user = window.Telegram?.WebApp?.initDataUnsafe?.user;
   // Закоментить строки ниже для тестов визуальной части в браузере
   // Параметры из тг там не появятся!
@@ -26,6 +49,7 @@ export default function App() {
       </div>
     );
   }
+
   if (!isAuthenticated) {
     return (
       <div className="fixed inset-0 z-9999 flex items-center justify-center bg-neutral-content">
@@ -35,18 +59,24 @@ export default function App() {
   }
   // Коментировать до этой строчки!
   // Далее идет код страницы
+  if (isAuth) {
+    return (
+      <>
+        {activeTab == 'resources' ? (
+          <Resources />
+        ) : activeTab == 'calendar' ? (
+          <Calendar />
+        ) : activeTab == 'profile' ? (
+          <Profile />
+        ) : (
+          ''
+        )}
+        <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
+      </>
+    );
+  }
   return (
-    <>
-      {activeTab == 'resources' ? (
-        <Resources />
-      ) : activeTab == 'calendar' ? (
-        <Calendar />
-      ) : activeTab == 'profile' ? (
-        <Profile />
-      ) : (
-        ''
-      )}
-      <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
-    </>
-  );
+    <><Spinner /></>
+  )
+
 }
--- frontend/src/shared/types/telegram.d.ts
+++ frontend/src/shared/types/telegram.d.ts
@@ -13,6 +13,17 @@ declare global {
           button_text_color?: string;
           secondary_bg_color?: string;
         };
+        initData?: {
+          user?: {
+            id: number;
+            first_name: string;
+            last_name?: string;
+            username?: string;
+            language_code?: string;
+            is_premium?: boolean;
+            photo_url?: string;
+          };
+        };
         initDataUnsafe?: {
           user?: {
             id: number;
